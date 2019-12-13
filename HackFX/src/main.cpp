43ee2cb2b234f38058dbc4fc35c95a0402641d7b
Niklas Weber <niklas.weber_mail@t-online.de>
2019-12-13 20:42:18 +0100

    migrate to platformio

--- HackFX/src/main.cpp
+++ HackFX/src/main.cpp
@@ -1,36 +1,8 @@
+#include <Arduino.h>
 #include "ControlDesk.h"
-#include <MIDI.h>
+#include "MIDI.h"
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial3,  MIDI);
-
-void setup() {
-
-  ControlDesk.registerButtonPressedCallBack(onButtonPressed);
-  ControlDesk.registerButtonReleasedCallBack(onButtonReleased);
-  ControlDesk.registerPotChangedCallBack(onPotValChanged);
-  
-  ControlDesk.setDeskLamp(255);
-  
-  Serial.begin(115200);
-  Serial.println("Start");
-
-  MIDI.begin(MIDI_CHANNEL_OMNI);
-
-  //by default all LEDs are HIGH
-  for(int i=0; i<32; i++){
-    ControlDesk.setLed(i, LOW);
-  }
-}
-
-/* ########################################################## */
-
-void loop() {
-
-  ControlDesk.update();
-
-  //TODO: Handle when power button is pressed
-  //TODO: eventually make PWM for LEDs possible (not enough memory?)
-}
 
 void onButtonPressed(Button *button){
   switch (button->getId()){
@@ -62,3 +34,31 @@ void onPotValChanged(Pot *pot){
   MIDI.sendControlChange(pot->getId(),pot->getValue(),5);
 }
 
+void setup() {
+
+  ControlDesk.registerButtonPressedCallBack(onButtonPressed);
+  ControlDesk.registerButtonReleasedCallBack(onButtonReleased);
+  ControlDesk.registerPotChangedCallBack(onPotValChanged);
+  
+  ControlDesk.setDeskLamp(255);
+  
+  Serial.begin(115200);
+  Serial.println("Start");
+
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+
+  //by default all LEDs are HIGH
+  for(int i=0; i<32; i++){
+    ControlDesk.setLed(i, LOW);
+  }
+}
+
+/* ########################################################## */
+
+void loop() {
+
+  ControlDesk.update();
+
+  //TODO: Handle when power button is pressed
+  //TODO: eventually make PWM for LEDs possible (not enough memory?)
+}
